25a23ac3250788af19959cba8d729b595bbbb6f3
myapos <myapos@gmail.com>
2025-05-18 02:21:23 +0300

    adds eruda

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -1,4 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+console.log("Renderer process started");
+const isDev = import.meta.env?.DEV ?? process.env.NODE_ENV === "development";
+
+console.log("Renderer process started", isDev);
+console.log("eee", process.env.NODE_ENV);
+console.log("ddd", import.meta.env);
+if (isDev) {
+  // Dynamically import eruda in development
+  import("eruda").then((eruda) => {
+    eruda.default.init();
+    console.log("Eruda initialized");
+  });
+}
+
 createApp(App).mount("#app");
