8e22f2ee6ebb43d1cd24a8cd850a16a061d5cd3f
myapos <myapos@gmail.com>
2025-05-18 00:10:50 +0300

    first commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Vue component {{ test }}
+    <button @click="increment">Click me</button>
+    <div>{{ counter }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const test = ref("Loaded");
+const counter = ref(0);
+
+const increment = () => {
+  counter.value++;
+};
+</script>
