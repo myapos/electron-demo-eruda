ba5d4af58f6ccd8e7b82ca3185d5d89de14d2538
myapos <myapos@gmail.com>
2025-05-18 03:16:32 +0300

    wip

--- src/renderer/main.ts
+++ src/renderer/main.ts
@@ -8,11 +8,23 @@ console.log("Renderer process started", isDev);
 console.log("eee", process.env.NODE_ENV);
 console.log("ddd", import.meta.env);
 if (isDev) {
+  console.log("mpika");
   // Dynamically import eruda in development
   import("eruda").then((eruda) => {
     eruda.default.init();
     console.log("Eruda initialized");
+    eruda.default.position({
+      x: window.innerWidth - 60,
+      y: window.innerHeight - 60,
+    });
   });
+  // const eruda = await import("eruda");
+  // eruda.default.init();
+  // console.log("Eruda initialized");
+  // eruda.default.position({
+  //   x: window.innerWidth - 60,
+  //   y: window.innerHeight - 60,
+  // });
 }
 
 createApp(App).mount("#app");
